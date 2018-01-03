3c91f7fb381f25dbed38fe9741205d41decb5027
Leon Schreiber <leon.schreiber@omikron.net>
2018-01-03 18:12:08 +0100

    Initial commit

---
+++ Wallet.Api/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wallet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController
+    {
+        private const string Token = "9b0279ac-8005-455a-99d5-9c6fe3eb0aec";
+
+        [HttpGet]
+        public async Task<string> GetAll(string mail)
+        {
+
+        }
+    }
+}
